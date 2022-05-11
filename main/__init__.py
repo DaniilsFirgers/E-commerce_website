2d5ab768a4eb9731a0054f58acdd8928a46c9021
dfirgers <dfirger@gmail.com>
2022-05-12 00:08:02 +0300

    Add gunicorn

--- main/__init__.py
+++ main/__init__.py
@@ -4,8 +4,9 @@ from typing import Callable
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SECRET_KEY'] = 'SUPER_SECRET_KEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///datebase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
